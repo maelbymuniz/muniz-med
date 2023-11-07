2ae9dfc6f2217ceb9d3352db5ca8f277410d90b7
Maelby Muniz <maelbymuniz@gmail.com>
2023-11-07 11:59:55 -0300

    Feat: Error 404 Handler

---
+++ src/main/java/muniz/med/api/infra/ErrorHandler.java
@@ -0,0 +1,17 @@
+package muniz.med.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404handler() {
+        return ResponseEntity.notFound().build();
+    }
+
+
+}
