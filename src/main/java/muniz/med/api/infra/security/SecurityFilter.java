17db13c526d1af60afdee5b9a8b6434c90837ef4
Maelby Muniz <maelbymuniz@gmail.com>
2023-11-15 12:53:30 -0300

    Edit: SecurityFilter and ErrorHandler

--- src/main/java/muniz/med/api/infra/security/SecurityFilter.java
+++ src/main/java/muniz/med/api/infra/security/SecurityFilter.java
@@ -5,7 +5,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import muniz.med.api.domain.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,12 +20,19 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = requestToken(request);
+
         if (tokenJWT != null) {
             var subject = tokenService.getSubject(tokenJWT);
+            var user = userRepository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
@@ -30,7 +41,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String requestToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null) {
-            return authorizationHeader.replace("Bearer", "");
+            return authorizationHeader.replace("Bearer ", "");
         }
 
         return null;
